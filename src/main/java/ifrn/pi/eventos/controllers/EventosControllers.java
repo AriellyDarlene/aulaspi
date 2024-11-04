8bd0293110f30c80502e9ab72da2826dc815de8f
Arielly Darlene da Silva e Silva <20211204010051@ifrn.local>
2024-11-04 17:04:54 -0300

    Criando a listagem de eventos

--- src/main/java/ifrn/pi/eventos/controllers/EventosControllers.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosControllers.java
@@ -1,41 +1,53 @@
 package ifrn.pi.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
+@RequestMapping("/eventos")
 public class EventosControllers {
 	
 	@Autowired
 	private EventoRepository er;
 
-	@RequestMapping("/eventos/form")
+	@GetMapping("/form")
 	public String form() {
-		return "formEvento";
+		return "eventos/formEvento";
 	}
 	
-	@PostMapping("/eventos")
+	@PostMapping
 	public String adicionar(Evento evento) {
 		
 		System.out.println(evento);
 		er.save(evento);
 		
-		return "evento-adicionado";
+		return "home";
 	}
 	
-	@RequestMapping("/eventos/submit")
-	public String submit(Evento evento) {
-		System.out.println("O método foi executado");
-		System.out.println(evento.getNome());
-		System.out.println(evento.getLocal());
-		System.out.println(evento.getData());
-		System.out.println(evento.getHorario());
-		return "ExecutandoSubmit";
+	@GetMapping
+	public ModelAndView listar() {
+		
+		List<Evento> eventos = er.findAll();
+		ModelAndView mv = new ModelAndView("eventos/lista");
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
+
 }
+
+
+//	@RequestMapping("/eventos/submit") public String submit(Evento evento) { System.out.println("O método foi executado"); System.out.println(evento.getNome()); 	System.out.println(evento.getLocal()); 	
+// System.out.println(evento.getData()); System.out.println(evento.getHorario()); return "ExecutandoSubmit"; }
+
+
+	
+	
